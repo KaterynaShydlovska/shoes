e53f373b9f60bbdecae8aee8fdfe76c172d1c680
katerynashydlovska <shydlovskayaka@gmail.com>
2019-09-24 20:36:15 -0700

    Playceholder

---
+++ test.js
@@ -0,0 +1,24 @@
+var username = prompt('Welcome! What is your name?');
+
+console.log('Hi' + username + '!');
+
+console.log('<p>Hi,' + username + '</p>');
+
+var shoesSize = prompt(' What is your shoes size?');
+
+console.log(shoesSize);
+
+document.write('<p>Your size is,' + shoesSize + '</p>');
+
+var wantsToOrder = prompt('Do you wanna order new kicks?');
+
+if (wantsToOrder === 'yes') {
+    document.write("here");
+    document.write('<p>We re so happy you choosed us. </p>');
+
+    var shoesStyle = prompt('What style of shoes you prefer?')
+
+    document.write('<p><img src="shoes/' + shoesStyle + '.jpg"</p > ');
+} else {
+    documen.write('<p>Thank you for stoping by. If you will have any questions go to contact page and we will happy to help you.</p>');
+}
