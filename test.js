cb5840201b3a5605f635e2fa3ee645dca73186e0
katerynashydlovska <shydlovskayaka@gmail.com>
2019-09-25 22:58:18 -0700

    Added new shoes

--- test.js
+++ test.js
@@ -1,26 +1,38 @@
+var sklad = ["Sneakers", "Running", "Athletic", "Slip-on"];
+
 var username = prompt('Welcome! What is your name?');
 
 console.log('Hi' + username + '!');
 
 document.write('<h1>Welcome,' + username + '!' + '</h1>');
 
-console.log('<p>Hi,' + username + '</p>');
+var answer = prompt("Do you want to buy shoes ?")
 
-var shoesSize = prompt(' What is your shoes size?');
+while ("yes" === answer) {
 
-console.log(shoesSize);
+    var shoes = prompt("What kind of shoe do you want ??");
 
-document.write('<p>Your size is ' + shoesSize + '</p>');
+    if (sklad.includes(shoes)) {
 
-var wantsToOrder = prompt('Do you wanna order new kicks?');
+        var shoesSize = prompt("what size of shoes do you need?")
 
-if (wantsToOrder === 'yes') {
+        document.write('<p>Your size is ' + shoesSize + '</p>');
 
-    document.write('<p>We are so happy that you choosed us. </p>');
-
-    var shoesStyle = prompt('What style of shoes you prefer?')
-
-    document.write('<p><img src="shoes/' + shoesStyle + '.jpg"</p > ');
-} else {
-    documen.write('<p>Thank you for stoping by. If you will have any questions go to contact page and we will happy to help you.</p>');
+        document.write('<p><img src="shoes/' + shoes + '.jpg"</p > ');
+    } else {
+        alert("sorry we dont have this shoes");
+    }
+    answer = prompt("Do you want to buy another shoes ?")
 }
+
+
+// if (wantsToOrder === 'yes') {
+
+//     document.write('<p>We are so happy that you choosed us. </p>');
+
+//     var shoesStyle = prompt('What style of shoes you prefer?')
+
+
+// } else {
+//     documen.write('<p>Thank you for stoping by. If you will have any questions go to contact page and we will happy to help you.</p>');
+// }
